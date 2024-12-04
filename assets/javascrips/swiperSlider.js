a431969656466e420207c2cc97c327ad82eaf63a
Taposh paul <taposhp05@gmail.com>
2024-12-04 21:45:40 +0600

    create responsive nav ber

--- assets/javascrips/swiperSlider.js
+++ assets/javascrips/swiperSlider.js
@@ -1,9 +1,50 @@
+// hero slider 
+
 new Swiper('.hero__slider', {
     // Optional parameters
     loop: true,
     speed: 1500,
     autoplay: {
-        delay: 5000,
-    },
+        delay: 2000,
+    }
 
+});
+
+// categories slider
+const categoriesCartsSlider = new Swiper(".categories__carts--slider", {
+    loop: true,
+    speed: 1000,
+    autoplay: {
+        delay: 2000, // Automatic sliding delay (in ms)
+        disableOnInteraction: false, // Keep autoplay active after user interaction
+    },
+    spaceBetween: 40,
+    breakpoints: {
+        1200: {
+            slidesPerView: 6,
+        },
+        992: {
+            slidesPerView: 5,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        480: {
+            slidesPerView: 1,
+        },
+    },
+})
+
+// Stop autoplay on hover
+const slides = document.querySelectorAll('.swiper-slide');
+slides.forEach((slide) => {
+    // Stop autoplay on hover
+    slide.addEventListener('mouseenter', () => {
+        categoriesCartsSlider.autoplay.stop();
+    });
+
+    // start autoplay on hover
+    slide.addEventListener('mouseleave', () => {
+        categoriesCartsSlider.autoplay.start();
+    });
 });
